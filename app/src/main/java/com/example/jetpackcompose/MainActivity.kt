9f808be2a06b2da94d0bfcb79c76f84cff7c135a
farshid <sputnick01.farshid75@gmail.com>
2024-04-27 23:15:19 +0330

    Text Style

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -26,6 +26,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -121,56 +127,81 @@ class MainActivity : ComponentActivity() {
 //                Icon(imageVector = Icons.Filled.Face, contentDescription = "", tint = Color.Red)
 //            }
 
-        var count by remember{
-            mutableStateOf(0)
-        }
+//        var count by remember{
+//            mutableStateOf(0)
+//        }
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .background(Color.White)
+//                .padding(10.dp),
+//            verticalArrangement = Arrangement.spacedBy(45.dp),
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//
+//            Button(
+//                onClick = { /*TODO*/ }, shape = RoundedCornerShape(10.dp)
+//            ) {
+//                Text(text = "RoundedCornerShape", fontSize = 15.sp)
+//            }
+//
+//            Button(
+//                onClick = { /*TODO*/ },
+//                shape = CutCornerShape(10.dp)
+//            ) {
+//                Text(text = "RoundedCornerShape", fontSize = 15.sp)
+//            }
+//            Button(
+//                onClick = { /*TODO*/ }, modifier = Modifier.size(50.dp),
+//                shape = CircleShape,
+//                elevation = ButtonDefaults.buttonElevation(
+//                    defaultElevation = 10.dp, pressedElevation = 6.dp
+//
+//                )
+//
+//            ) {
+//                Icon(imageVector = Icons.Filled.Add, contentDescription = "",
+//                    modifier=Modifier.size(30.dp))
+//            }
+//            Button(
+//                onClick = { count++ }, shape = RoundedCornerShape(10.dp),
+//                elevation = ButtonDefaults.buttonElevation(
+//                    defaultElevation = 10.dp,
+//                    pressedElevation = 6.dp
+//
+//                ), colors = ButtonDefaults.buttonColors(
+//
+//                    containerColor = Color(0xfffedbd0),
+//                    contentColor = Color.Cyan
+//                )
+//            ) {
+//                Text(text = "$count", fontSize = 15.sp)
+//            }
+//        }
+
+
+//        Text Style
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color.White)
-                .padding(10.dp),
-            verticalArrangement = Arrangement.spacedBy(45.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            Button(
-                onClick = { /*TODO*/ }, shape = RoundedCornerShape(10.dp)
-            ) {
-                Text(text = "RoundedCornerShape", fontSize = 15.sp)
-            }
+            val myFont= FontFamily(Font(R.font.aldrich))
+            Text(
+                stringResource(id = R.string.sample_text).repeat(10),
+                fontSize = 18.sp,
+                fontFamily = myFont,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.Red,
+                maxLines = 10,
+                overflow = TextOverflow.Ellipsis
 
-            Button(
-                onClick = { /*TODO*/ },
-                shape = CutCornerShape(10.dp)
-            ) {
-                Text(text = "RoundedCornerShape", fontSize = 15.sp)
-            }
-            Button(
-                onClick = { /*TODO*/ }, modifier = Modifier.size(50.dp),
-                shape = CircleShape,
-                elevation = ButtonDefaults.buttonElevation(
-                    defaultElevation = 10.dp, pressedElevation = 6.dp
 
-                )
+            )
 
-            ) {
-                Icon(imageVector = Icons.Filled.Add, contentDescription = "",
-                    modifier=Modifier.size(30.dp))
-            }
-            Button(
-                onClick = { count++ }, shape = RoundedCornerShape(10.dp),
-                elevation = ButtonDefaults.buttonElevation(
-                    defaultElevation = 10.dp,
-                    pressedElevation = 6.dp
-
-                ), colors = ButtonDefaults.buttonColors(
-
-                    containerColor = Color(0xfffedbd0),
-                    contentColor = Color.Cyan
-                )
-            ) {
-                Text(text = "$count", fontSize = 15.sp)
-            }
         }
 
 
